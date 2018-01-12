661c501f8309fc0f1bcda4e2beed41d50b43e0c8
wanjian <foreverzqh@foxmail.com>
2018-01-12 16:36:15 +0800

    no opt lib

---
+++ saknop/src/main/java/com/wanjian/sak/SAK.java
@@ -0,0 +1,38 @@
+package com.wanjian.sak;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.wanjian.sak.config.Config;
+
+/**
+ * Created by wanjian on 2017/2/20.
+ */
+
+public class SAK {
+
+    public static final int INFO_KEY = 0;
+
+
+    public static synchronized void init(Application application) {
+    }
+
+    public static synchronized void init(Application application, Config config) {
+
+
+    }
+
+
+    public static void resume(Activity activity) {
+
+    }
+
+    public static void pause(Activity activity) {
+
+    }
+
+    public synchronized static void unInstall(Application application) {
+
+    }
+
+}
---
+++ saknop/src/main/java/com/wanjian/sak/config/Config.java
@@ -0,0 +1,10 @@
+package com.wanjian.sak.config;
+
+/**
+ * Created by wanjian on 2017/2/20.
+ */
+
+public class Config {
+
+
+}
